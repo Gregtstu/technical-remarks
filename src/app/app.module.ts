b163c86e35e763c6b5cf08a71782fd0dc34e768b
Gregtstu <boltnev.grisha@yandex.ru>
2022-11-13 13:00:20 +0300

    added material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { EditPageComponent } from './pages/edit-page/edit-page.component';
 import { FavoritePageComponent } from './pages/favorite-page/favorite-page.component';
 import { PostPageComponent } from './pages/post-page/post-page.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { MainPageComponent } from './pages/main-page/main-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
