6e76f15a5ca3984fe7898f1246081e29162ffe49
Gregtstu <boltnev.grisha@yandex.ru>
2022-11-13 12:49:46 +0300

    added structure site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { CreatePageComponent } from './pages/create-page/create-page.component';
+import { EditPageComponent } from './pages/edit-page/edit-page.component';
+import { FavoritePageComponent } from './pages/favorite-page/favorite-page.component';
+import { AllPostsComponent } from './pages/all-posts/all-posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    CreatePageComponent,
+    EditPageComponent,
+    FavoritePageComponent,
+    AllPostsComponent
   ],
   imports: [
     BrowserModule,
